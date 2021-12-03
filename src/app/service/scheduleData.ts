fa54373ac729995bbc6f9d4297e49d7519fc69ae
Bagus Saptoaji <bagussaptoaji@gmail.com>
2021-12-03 13:51:49 +0700

    update terakhir

--- src/app/service/scheduleData.ts
+++ src/app/service/scheduleData.ts
@@ -3,6 +3,7 @@ import { Team } from "../interface/team";
 
 
 export const SEASON_SCHEDULE:Schedule[] = [
+
     {id:1,playingDate:new Date(2021,8,10),HomeTeam: 'Persija',
     AwayTeam: 'Persikabo',HomeScore:3,AwayScore:2,RefName: "Joko",
     notes:'Pertandingan Overtime'},
@@ -12,11 +13,11 @@ export const SEASON_SCHEDULE:Schedule[] = [
     {id:3,playingDate:new Date(2021,8,12),HomeTeam: 'Persipura',
     AwayTeam: 'Persib',HomeScore:3,AwayScore:2,RefName: "Parto",
     notes:'Pertandingan Overtime'},
-    {id:4,playingDate:new Date(2021,8,13),HomeTeam: 'PSM',
-    AwayTeam: 'Persisam',HomeScore:3,AwayScore:2,RefName: "Sule",
+    {id:4,playingDate:new Date(2021,8,13),HomeTeam: 'Persija',
+    AwayTeam: 'Persib',HomeScore:3,AwayScore:2,RefName: "Sule",
     notes:'Pertandingan Ada Kericuhan'},
-    {id:5,playingDate:new Date(2021,8,14),HomeTeam: 'Mitra Kukar',
-    AwayTeam: 'Persebaya',HomeScore:3,AwayScore:2,RefName: "Joki",
+    {id:5,playingDate:new Date(2021,8,14),HomeTeam: 'Persija',
+    AwayTeam: 'Persikabo',HomeScore:3,AwayScore:2,RefName: "Joki",
     notes:'Pertandingan Aman'},
 ]
 export const TEAMS:Team [] = [
@@ -26,8 +27,4 @@ export const TEAMS:Team [] = [
     {id:4,name:'Arema',type:'Over 30'},
     {id:5,name:'Persipura',type:'Over 30'},
     {id:6,name:'Persib',type:'Over 30'},
-    {id:7,name:'Persisam',type:'Over 30'},
-    {id:8,name:'PSM',type:'Over 30'},
-    {id:9,name:'Mitra Kukar',type:'Over 30'},
-    {id:10,name:'Persebaya',type:'Over 30'}
 ]
