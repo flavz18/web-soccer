4fdf13077ae9fa6725bb886856454b8ff3b98c5f
Bagus Saptoaji <bagussaptoaji@gmail.com>
2021-12-03 13:28:56 +0700

    hampir selesai

--- src/app/scorings/scorings.component.ts
+++ src/app/scorings/scorings.component.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ViewEncapsulation } from '@angular/core';
+import { Schedule } from '../interface/schedule';
+import { SoccerService } from '../service/SoccerService';
 
 @Component({
   selector: 'app-scorings',
   templateUrl: './scorings.component.html',
   styleUrls: ['./scorings.component.css']
 })
-export class ScoringsComponent implements OnInit {
 
-  constructor() { }
+export class ScoringsComponent implements OnInit {
+  private UsingAsync:boolean = false;
+  private CurGame:number = 0;
+  public MySchedule: Schedule[];
+  public LeagueName: string;
+  public HomeTeam: string;
+  public AwayTeam: string;
+  public HomeScore : number = 0;
+  public AwayScore : number = 0;
+  public SeasonStart:Date = new Date;
+  public CurrentRole:number = 1;
+  constructor(private _soccerService: SoccerService) {
+    this.LeagueName= "Wakanda League";
+    this.MySchedule = [];
+    this.HomeTeam = "";
+    this.AwayTeam = "";
+    this.getSchedule();
+    this.SeasonStart.setTime(this.SeasonStart.getTime()+4 * 86400000);
+    if(this.MySchedule.length>0){
+      this.UpdVariables(0); //Default First Game
+      this.CurGame = 1;
+    }
+
+  }
 
   ngOnInit(): void {
   }
 
+  public onSchedChange(GameValue:number){
+    if(GameValue>0){
+      this.UpdVariables(GameValue);
+      this.CurGame = GameValue;
+    }
+  }
+  public onRecordScores(){
+    this.MySchedule[this.CurGame-1].AwayScore = Number(this.AwayScore);
+    this.MySchedule[this.CurGame-1].HomeScore = Number(this.HomeScore);
+  }
+  //Update Screen Variable Based On Current Game
+  private UpdVariables(GameID:number){
+    //Need to Search Schedule array,looking for Game Id
+    var x : number = 0;
+    if(GameID > 0){
+      x = GameID-1;
+    }
+    this.HomeTeam = this.MySchedule[x].HomeTeam;
+    this.AwayTeam = this.MySchedule[x].AwayTeam;
+    this.HomeScore = this.MySchedule[x].HomeScore;
+    this.AwayScore = this.MySchedule[x].AwayScore;
+  }
+
+  //get the schedule only showing games not yet scored
+  private getSchedule(){
+    if(this.UsingAsync){
+      let xx = this._soccerService.getScheduleAsync();
+    }else{
+      this.MySchedule = this._soccerService.getSchedule();
+    }
+  }
 }
--- src/app/service/scheduleData.ts
+++ src/app/service/scheduleData.ts
@@ -3,28 +3,31 @@ import { Team } from "../interface/team";
 
 
 export const SEASON_SCHEDULE:Schedule[] = [
-    {id:1,playingDate:new Date(2021,08,10),HomeTeam: 'Persija',
-    AwayTeam: 'Bali United',HomeScore:3,AwayScore:2,RefName: "Joko",
+    {id:1,playingDate:new Date(2021,8,10),HomeTeam: 'Persija',
+    AwayTeam: 'Persikabo',HomeScore:3,AwayScore:2,RefName: "Joko",
     notes:'Pertandingan Overtime'},
-    {id:2,playingDate:new Date(2021,08,11),HomeTeam: 'EVOS',
-    AwayTeam: 'Persipon',HomeScore:3,AwayScore:2,RefName: "Kasjo",
-    notes:'Pertandingan Ngelek'},
-    {id:3,playingDate:new Date(2021,08,12),HomeTeam: 'Persis',
+    {id:2,playingDate:new Date(2021,8,11),HomeTeam: 'Bali United',
+    AwayTeam: 'Arema',HomeScore:3,AwayScore:2,RefName: "Kasjo",
+    notes:'Pertandingan Santai'},
+    {id:3,playingDate:new Date(2021,8,12),HomeTeam: 'Persipura',
     AwayTeam: 'Persib',HomeScore:3,AwayScore:2,RefName: "Parto",
     notes:'Pertandingan Overtime'},
-    {id:4,playingDate:new Date(2021,08,13),HomeTeam: 'Persib',
-    AwayTeam: 'Persija',HomeScore:3,AwayScore:2,RefName: "Sule",
+    {id:4,playingDate:new Date(2021,8,13),HomeTeam: 'PSM',
+    AwayTeam: 'Persisam',HomeScore:3,AwayScore:2,RefName: "Sule",
     notes:'Pertandingan Ada Kericuhan'},
-    {id:5,playingDate:new Date(2021,08,14),HomeTeam: 'Persib',
+    {id:5,playingDate:new Date(2021,8,14),HomeTeam: 'Mitra Kukar',
     AwayTeam: 'Persebaya',HomeScore:3,AwayScore:2,RefName: "Joki",
     notes:'Pertandingan Aman'},
 ]
-export const TEAM:Team [] = [
-    {id:1,name:'EVOS',type:'Over 30'},
-    {id:2,name:'Persija',type:'Over 30'},
+export const TEAMS:Team [] = [
+    {id:1,name:'Persija',type:'Over 30'},
+    {id:2,name:'Persikabo',type:'Over 30'},
     {id:3,name:'Bali United',type:'Over 30'},
-    {id:4,name:'Persipon',type:'Over 30'},
-    {id:5,name:'Persis',type:'Over 30'},
+    {id:4,name:'Arema',type:'Over 30'},
+    {id:5,name:'Persipura',type:'Over 30'},
     {id:6,name:'Persib',type:'Over 30'},
-    {id:7,name:'Persebaya',type:'Over 30'}
+    {id:7,name:'Persisam',type:'Over 30'},
+    {id:8,name:'PSM',type:'Over 30'},
+    {id:9,name:'Mitra Kukar',type:'Over 30'},
+    {id:10,name:'Persebaya',type:'Over 30'}
 ]
